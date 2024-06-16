2bb8a3a67b5a968d50124674a487b5987095469c
rajput999 <rajput9997647419@gmail.com>
2024-06-16 19:14:11 +0530

    deploy

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
