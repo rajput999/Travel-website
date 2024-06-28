0a93963832c7ed7683aa77ad314e44afb1c2fb7e
rajput999 <rajput9997647419@gmail.com>
2024-06-28 20:43:49 +0530

    nodemailer and other

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,43 @@
+// Import Dependencies
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
+// Route Imports
 const { connectToMongoDB } = require("./connection");
 const homeRoute = require("./routes/home");
 const userRoute = require("./routes/user");
+const {
+  handleCreateBookingById,
+  handleDeleteBookingById,
+} = require("./controllers/authBooking");
+const {
+  sendVerificationEmail,
+  handleVerifyEmail,
+  handleUserSignup,
+  handleUserSignin,
+  GiveTokens,
+  giveUserForToken,
+} = require("./controllers/authUser");
+const {
+  restrictedToLoggedinUsersOnly,
+  checkAuth,
+} = require("./middlewares/auth");
+
+
 
 const app = express();
 const PORT = 8000;
 
 // connection
 connectToMongoDB("mongodb://localhost:27017/travel-website").then(() =>
-    console.log("MongoDB Connected")
+  console.log("MongoDB Connected")
 );
 
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 
-// Specify the directory where your views are located 
+// Specify the directory where your views are located
 app.set("views", path.resolve("./views"));
 
 // Middleware
@@ -24,11 +46,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Router
-/*
-    -> "/"
-    -> "/user"
-*/
-app.use("/", homeRoute);
-app.use("/user", userRoute);
+
+
+// Testing Routes
+/**/ 
+app.use("/handleUserSignup",handleUserSignup);
+app.use("/handleUserSignin",handleUserSignin);
+app.use("/handleCreateBookingById,", restrictedToLoggedinUsersOnly,handleCreateBookingById);
+app.use("/handleDeleteBookingById", restrictedToLoggedinUsersOnly,handleDeleteBookingById);
+app.use("/sendVerificationMail", sendVerificationEmail);
+app.use("/handleVerifyEmail", handleVerifyEmail);
+app.use("/GiveToken", GiveTokens);
+app.use("/giveUserForToken", giveUserForToken);
+
+/**/
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   from: { type: String, required: true },
   to: { type: String, required: true },
   date: { type: Date, required: true },
+  createdBy: {type: String, required: true},
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  isTraveled: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
